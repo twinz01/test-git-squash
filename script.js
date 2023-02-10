135c3a962268dd929919287d10cce47a53bf13c4
twinz01 <arylzan.astara@gmail.com>
2023-02-10 19:39:01 +0700

    Add calculator function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Test");
+
+function calculate(i, operator, j) {
+  let k;
+  switch (operator) {
+    case "+":
+      k = i + j;
+      break;
+    case "-":
+      k = i - j;
+      break;
+    case "*":
+      k = i * j;
+      break;
+    case "/":
+      k = i / j;
+  }
+  alert("The result is " + k);
+}
